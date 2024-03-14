1e420c09d03152e68343a48efc952bdec318593e
SantiV03 <Santivelgasss24@gmail.com>
2024-03-14 19:18:31 -0300

    Eliminar producto
    Eliminar producto

---
+++ src/app/modules/product/product/product/product.component.ts
@@ -0,0 +1,140 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material/snack-bar';
+import { MatTableDataSource } from '@angular/material/table';
+import { subscribeOn } from 'rxjs';
+import { ProductService } from 'src/app/modules/shared/services/product.service';
+import { NewProductComponent } from '../../new-product/new-product.component';
+import { ConfirmComponent } from 'src/app/modules/shared/components/confirm/confirm.component';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  constructor(private productService: ProductService,
+    public dialog: MatDialog, private snackBar: MatSnackBar,) { }
+
+  ngOnInit(): void {
+    this.getProducts()
+  }
+
+  displayedColumns: string[] = ['id', 'name', 'price', 'account', 'category', 'picture', 'actions'];
+  dataSource = new MatTableDataSource<ProductElement>();
+
+  @ViewChild(MatPaginator)
+  paginator! : MatPaginator
+
+  getProducts(){
+    this.productService.getProducts()
+    .subscribe((data:any)=> {
+      console.log("respuesta de productos", data)
+      this.proccesProductResponse(data)
+    }, (error: any) => {
+      console.log("error en productos", error)
+    })
+    
+  }
+  
+
+  proccesProductResponse(resp: any) {
+    const dateProduct: ProductElement[] = [];
+    if (resp.metadata[0].code == "00") {
+      let listCProduct = resp.product.products;
+  
+      listCProduct.forEach((element: ProductElement) => {
+        element.picture = 'data:image/jpeg;base64,' + element.picture;
+        dateProduct.push(element);
+      });
+  
+      this.dataSource = new MatTableDataSource<ProductElement>(dateProduct);
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
+  openProductDialog(){
+    const dialogRef = this.dialog.open(NewProductComponent  , {
+      width: '450px'
+    });
+
+    dialogRef.afterClosed().subscribe((result:any) => {
+      if(result == 1){
+        this.openSnackBar("Producto Agregado", "Exitosa");
+        this.productService.getProducts();
+      } else if (result == 2){
+        this.openSnackBar("Producto NO guardado", "Fallida");
+      }
+      
+    });
+  }
+
+  openSnackBar(massage: string, action: string) : MatSnackBarRef<SimpleSnackBar>{
+    return this.snackBar.open(massage, action, {
+      duration: 2000
+    })
+
+  }
+
+  edit(id: number, name: string, price: number, account: number, category: any){
+    const dialogRef = this.dialog.open(NewProductComponent  , {
+      width: '450px',
+      data: {id: id, name: name, price: price, account: account, category: category}
+    });
+
+    dialogRef.afterClosed().subscribe((result:any) => {
+      if(result == 1){
+        this.openSnackBar("Producto editado", "Exitosa");
+        this.productService.getProducts();
+      } else if (result == 2){
+        this.openSnackBar("Producto NO editado", "Error");
+      }
+      
+    });
+  }
+
+  delete(id:any){
+    const dialogRef = this.dialog.open(ConfirmComponent  , {
+      width: '450px',
+      data: {id: id, module: "product"}
+    });
+
+    dialogRef.afterClosed().subscribe((result:any) => {
+      if(result == 1){
+        this.openSnackBar("Producto eliminado", "Exitosa");
+        this.productService.getProducts();
+      } else if (result == 2){
+        this.openSnackBar("Producto NO elimidano", "Error");
+      }
+      
+    });
+  }
+
+  buscar(name:any): void{
+    if (name.length ===0){
+      return this.getProducts()
+    }
+
+    this.productService.getProductsByName(name)
+      .subscribe((resp:any)=>{
+        this.proccesProductResponse(resp)
+      })
+  }
+
+}
+
+
+
+export interface ProductElement {
+  id: number
+  name: string
+  price: number
+  account: number
+  category: any
+  picture: any
+ 
+}
+
+
---
+++ src/app/modules/shared/services/product.service.ts
@@ -0,0 +1,63 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const base_url = environment.base_url
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+
+
+  getProducts(){
+
+    const endpoint =  `${base_url}/products`;
+    return this.http.get(endpoint);
+    
+  }
+
+  /**
+   * Guardar producto
+   */
+  
+  saveProduct(body: any){
+    const endpoint =  `${base_url}/products`;
+    return this.http.post(endpoint, body);
+
+  }
+
+
+  /**
+   * Actualizar producto
+   */
+
+  updateProduct (body:any, id:any){
+    const endpoint =  `${base_url}/products/${id}`
+    return this.http.put(endpoint, body)
+  }
+
+  /**
+   * Eliminar producto
+   */
+
+  deleteProduct(id:any){
+    const endpoint =  `${base_url}/products/${id}`
+    return this.http.delete(endpoint)
+  }
+
+  /**
+   * buscar producto por nombre
+   */
+  
+  getProductsByName(name:any){
+    const endpoint =  `${base_url}/products/filter/${name}`
+    return this.http.get(endpoint)
+  }
+
+  }
+
+
+
